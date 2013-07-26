9e397370f884928f9f64bae369699be18b956702
Matt Dotson <matt.dotson@gmail.com>
2013-07-26 10:55:55 -0700

    platform

---
+++ src/PlatformAdapter/Platform.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PlatformAdapter
+{
+    /// <summary>
+    /// we should use a real dependency injection framework/implementation or MEF.  This is a hack
+    /// </summary>
+    public class Platform
+    {
+        private static readonly Platform current = new Platform();
+
+        public static Platform Current
+        {
+            get
+            {
+                return Platform.current;
+            }
+        }
+
+        private Platform()
+        {
+
+        }
+
+        private Dictionary<Type, Type> typeMap = new Dictionary<Type, Type>();
+
+        public void Register<TInterface, TImplementation>() where TImplementation : new()
+        {
+            //todo: make sure implementation implements interface
+
+            this.typeMap[typeof(TInterface)] = typeof(TImplementation);
+        }
+
+        public T GetAdapter<T>()
+        {
+            Type imp;
+
+            if (this.typeMap.TryGetValue(typeof(T), out imp))
+            {
+                return (T) Activator.CreateInstance(imp);
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+    }
+}
