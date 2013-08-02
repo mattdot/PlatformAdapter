f0902c3fa8710c58a7802367d1e36a3bffc157d8
Matt Dotson <matt.dotson@gmail.com>
2013-08-01 23:26:52 -0700

    added nupkg for 0.1.3

---
+++ src/PlatformAdapter/IServiceLocator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlatformAdapter
+{
+    public interface IServiceLocator
+    {
+        void RegisterType<TInterface, TImplementation>() where TImplementation : TInterface, new();
+        void RegisterType<T>(Func<T> factory);
+        void RegisterInstance<T>(T instance);
+        T Resolve<T>();
+    }
+}
---
+++ src/PlatformAdapter/SimpleServiceLocator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlatformAdapter
+{
+    internal sealed class SimpleServiceLocator : IServiceLocator
+    {
+        private Dictionary<Type, object> factories = new Dictionary<Type, object>();
+
+        public void RegisterType<TInterface, TImplementation>() where TImplementation : TInterface, new() 
+        {
+            this.RegisterType<TInterface>(() => (TInterface)(new TImplementation()));
+        }
+
+        public void RegisterType<T>(Func<T> factory)
+        {
+            this.factories[typeof(T)] = factory;
+        }
+
+        public void RegisterInstance<T>(T instance)
+        {
+            this.factories[typeof(T)] = (Func<T>)(() => instance);
+        }
+
+        public T Resolve<T>()
+        {
+            object f;
+
+            if (this.factories.TryGetValue(typeof(T), out f))
+            {
+                return ((Func<T>)f)();
+            }
+
+            return default(T);
+        }
+    }
+}
